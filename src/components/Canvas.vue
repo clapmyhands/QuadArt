3294d6b480483a5e9f6dd2d3372432d2cc2ce751
clapmyhands <pineapplepie90@gmail.com>
2024-03-27 01:37:11 +0800

    refactor(canvas): change to typescript

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,5 +1,13 @@
 <script lang="ts">
+interface Color {
+    r: number
+    g: number
+    b: number
+}
+
 class QuadNode {
+    static counter: number = 0;
+    id: number;
     error: number;
     color: string;
     prevColor: string;
@@ -10,15 +18,29 @@ class QuadNode {
     leaf: any;
     leafSize: number;
 
-    constructor(context2D: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, prevColor: string, leafSize: number) {
+    static GenerateID(): number {
+        return QuadNode.counter++;
+    }
+
+    static ResetID() {
+        QuadNode.counter = 0;
+    }
+
+    constructor(
+        context2D: CanvasRenderingContext2D,
+        x: number, y: number, w: number, h: number,
+        leafSize: number,
+        prevColor: string = "#000000",
+    ) {
         const data = context2D.getImageData(x, y, w, h).data;
         const color = this.calcAverageColor(data);
         const mse = this.calcColorMSE(data, color)
         const colorHex = this.colorToHexString(color);
 
+        this.id = QuadNode.GenerateID();
         this.error = mse;
         this.color = colorHex;
-        this.prevColor = prevColor ?? "#000000";
+        this.prevColor = prevColor;
         this.x = x
         this.y = y
         this.w = w
@@ -74,56 +96,14 @@ class QuadNode {
         const x2 = this.x + halfW;
         const y1 = this.y;
         const y2 = this.y + halfH;
-        
+
         return [
-            new QuadNode(context2D, x1, y1, halfW, halfH, this.color, this.leafSize),
-            new QuadNode(context2D, x2, y1, halfW, halfH, this.color, this.leafSize),
-            new QuadNode(context2D, x1, y2, halfW, halfH, this.color, this.leafSize),
-            new QuadNode(context2D, x2, y2, halfW, halfH, this.color, this.leafSize),
+            new QuadNode(context2D, x1, y1, halfW, halfH, this.leafSize, this.color),
+            new QuadNode(context2D, x2, y1, halfW, halfH, this.leafSize, this.color),
+            new QuadNode(context2D, x1, y2, halfW, halfH, this.leafSize, this.color),
+            new QuadNode(context2D, x2, y2, halfW, halfH, this.leafSize, this.color),
         ]
     }
-}
-
-function rgbToHex(r: number, g: number, b: number) {
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + (b)).toString(16).slice(1);
-}
-
-interface Color {
-    r: number
-    g: number
-    b: number
-}
-
-function se(c1: Color, c2: Color) {
-    const rErr = c1.r - c2.r;
-    const gErr = c1.g - c2.g;
-    const bErr = c1.b - c2.b;
-    return 0.2989 * (rErr * rErr) + 0.5870 * (gErr * gErr) + 0.1140 * (bErr * bErr);
-}
-
-function calcAverageColor(imageData: Uint8ClampedArray) {
-    let cumR = 0, cumG = 0, cumB = 0;
-    for (let i = 0; i < imageData.length; i += 4) {
-        cumR += imageData[i];
-        cumG += imageData[i + 1];
-        cumB += imageData[i + 2];
-    }
-    const area = imageData.length / 4;
-    const avgR = Math.round(cumR / area);
-    const avgG = Math.round(cumG / area);
-    const avgB = Math.round(cumB / area);
-    return { avgR, avgG, avgB }
-}
-
-function calcColorMSE(imageData: Uint8ClampedArray, color: Color) {
-    let mse = 0;
-    for (let i = 0; i < imageData.length; i += 4) {
-        mse += se(
-            color,
-            { r: imageData[i], g: imageData[i + 1], b: imageData[i + 2] }
-        )
-    }
-    return Math.sqrt(mse);
 }
 
 function convertRemToPixel(rem: number): number {
@@ -132,8 +112,9 @@ function convertRemToPixel(rem: number): number {
 </script>
 <script setup lang="ts">
 import { ref, onMounted, watch } from 'vue';
-import type {Ref} from 'vue'
+import type { Ref } from 'vue'
 import { select, interpolate } from 'd3';
+import { Selection } from 'd3';
 import { computed } from '@vue/reactivity';
 
 interface Parameter {
@@ -165,10 +146,8 @@ const props = withDefaults(defineProps<Props>(), {
 const img = new Image();
 const canvas: Ref<HTMLCanvasElement | null> = ref(null);
 let context: CanvasRenderingContext2D | null = null;
-let svg = null;
-let quads = ref([]);
-let tmp : Ref<QuadNode[]> = ref([]);
-let id = 0;
+let svg: Selection<SVGGElement, QuadNode, HTMLElement, any> | null = null;
+let tmp: Ref<QuadNode[]> = ref([]);
 let errorVal = ref('0');
 
 let updateModelTimer: number | null = null;
@@ -177,28 +156,11 @@ const updateViewFreq = 200;
 const updateModelFreq = 1;
 
 const errorInfo = computed(() => {
-    const iterations = Math.max((quads.value.length - 1) / 3, 0);
+    const iterations = Math.max((tmp.value.length - 1) / 3, 0);
     return 'Iterations: ' + iterations +
-        ' - Shapes: ' + quads.value.length +
+        ' - Shapes: ' + tmp.value.length +
         ' - Error: ' + errorVal.value;
 })
-
-function createQuadNode(x: number, y: number, width: number, height: number, prevColor: string) {
-    id++;  // dont use global id?
-    const data = context.getImageData(x, y, width, height).data;  // dont use global context?
-    const { avgR, avgG, avgB } = calcAverageColor(data);
-    const mse = calcColorMSE(data, { r: avgR, g: avgG, b: avgB })
-    const colorHex = rgbToHex(avgR, avgG, avgB);
-
-    return {
-        id: id,
-        error: mse,
-        color: colorHex,
-        prevColor: prevColor,
-        x: x, y: y, width: width, height: height,
-        leaf: width < props.param.leafSize || height < props.param.leafSize,
-    };
-}
 
 function reset() {
     const canvasE = document.getElementsByClassName('canvas');
@@ -221,51 +183,41 @@ function reset() {
         height = maxHeight
         width = Math.round(height * ratio);
     }
-    console.log(parentE.clientWidth, parentE.clientHeight);
-    console.log(width, height);
+    // console.log(parentE.clientWidth, parentE.clientHeight);
+    // console.log(width, height);
 
-    svg.attr('viewBox', '0 0 ' + width + ' ' + height)
+    if (context === null) {
+        console.log("context is null???")
+        return
+    }
+
+    svg?.attr('viewBox', '0 0 ' + width + ' ' + height)
         .attr('width', width)
         .attr('height', height)
         .style('background-color', props.param.backgroundColor);
+
+    // reset Canvas context
     context.canvas.width = width;
     context.canvas.height = height;
     context.drawImage(img, 0, 0, width, height);
 
-    quads.value = [];
-    quads.value.push(createQuadNode(0, 0, width, height, '#000000'));
+    // reset QuadNode stoere
+    tmp.value = [];
+    tmp.value.push(new QuadNode(context, 0, 0, width, height, props.param.leafSize));
+
+    // reset errorVal
     errorVal.value = '0';
 
     redraw();
 }
 
-function split(node) {
-    if (node.leaf) {
-        return
-    }
-    const idx = quads.value.indexOf(node);
-    quads.value.splice(idx, 1);
-
-    const halfW = node.width / 2;
-    const halfH = node.height / 2;
-    const x1 = node.x;
-    const x2 = node.x + halfW;
-    const y1 = node.y;
-    const y2 = node.y + halfH;
-    quads.value.push(
-        createQuadNode(x1, y1, halfW, halfH, node.color),
-        createQuadNode(x2, y1, halfW, halfH, node.color),
-        createQuadNode(x1, y2, halfW, halfH, node.color),
-        createQuadNode(x2, y2, halfW, halfH, node.color)
-    );
-}
-
 function step() {
     // can use quadtree instead of filtering array
-    const nonLeafNodes = quads.value.filter(n => !n.leaf)
+    const nonLeafNodes = tmp.value.filter(n => !n.leaf)
     if (nonLeafNodes.length <= 0) {
         return
     }
+    // use heap
     let maxE = nonLeafNodes[0];
     for (let i = 1; i < nonLeafNodes.length; i++) {
         const e = nonLeafNodes[i];
@@ -277,41 +229,34 @@ function step() {
         // emit('errorThresholdReached');
         return
     }
-    split(maxE);
-    errorVal.value = maxE.error.toPrecision(5);
-}
 
-function tmpStep() {
-    const nodes = tmp.value
-    if (nodes.length <= 0) {
-        return
+    tmp.value.splice(tmp.value.indexOf(maxE), 1);
+    const newNodes = maxE.Split(context!);
+    for (let i = 0; i < newNodes.length; i++) {
+        const node = newNodes[i];
+        // if (!node.leaf) {
+            // tmp.value.push(node);
+        // }
+        tmp.value.push(node);
     }
-    // use heap
-    let maxE = nodes[0];
-    for (let i = 1; i < nodes.length; i++) {
-        const e = nodes[i];
-        if (e.error > maxE.error) {
-            maxE = e
-        }
-    }
-    if (maxE.error < props.param.errorThreshold) {
-        // emit('errorThresholdReached');
-        return
-    }
-    maxE.Split(context!)
     errorVal.value = maxE.error.toPrecision(5);
 }
 
 function redraw(highlight: boolean = false) {
-    const rect = svg.selectAll('rect').data(quads.value, n => n.id);
+    if (svg === null) {
+        return
+    }
+    const rect: Selection<SVGRectElement, QuadNode, SVGGElement, any> = svg
+        .selectAll<SVGRectElement, QuadNode>('rect')
+        .data(tmp.value, n => n.id);
     rect.exit().remove();
     rect.enter()
         .append('rect')
         .attr('x', n => { return n.x + 0.25 })
         .attr('y', n => { return n.y + 0.25 })
         .attr('rx', props.param?.roundedCorner || 0)
-        .attr('width', n => { return n.width - 0.5 })
-        .attr('height', n => { return n.height - 0.5 })
+        .attr('width', n => { return n.w - 0.5 })
+        .attr('height', n => { return n.h - 0.5 })
         .attr('fill', n => { return highlight ? '#ffffff' : n.prevColor })
         .transition().duration(500).styleTween('fill', n => {
             return interpolate(highlight ? '#ffffff' : n.prevColor, n.color);
@@ -360,16 +305,48 @@ function save() {
         tmpContext!.drawImage(copyImg, 0, 0, w, h);
 
         // save canvas
-        let png = tmpCanvas.toDataURL();
-        let download = function (href: string, name: string) {
+        const saveCanvas = async (canvas: HTMLCanvasElement, suggestedName: string) => {
+            // Feature detection. The API needs to be supported
+            // and the app not run in an iframe.
+            const supportsFileSystemAccess = 'showSaveFilePicker' in window && (() => {
+                try {
+                    return window.self === window.top;
+                } catch {
+                    return false;
+                }
+            })();
+            // If the File System Access API is supported…
+            if (supportsFileSystemAccess) {
+                try {
+                    // Show the file save dialog.
+                    const handle = await window.showSaveFilePicker({
+                        suggestedName,
+                    });
+                    // Write the blob to the file.
+                    const writable = await handle.createWritable();
+                    canvas.toBlob(async (blob) => {
+                        await writable.write(blob!)
+                        await writable.close();
+                    })
+                    return;
+                } catch (err) {
+                    // Fail silently if the user has simply canceled the dialog.
+                    if (err instanceof DOMException && err.name !== 'AbortError') {
+                        console.error(err.name, err.message);
+                        return;
+                    }
+                    return;
+                }
+            }
+
             const link = document.createElement('a');
-            link.download = name;
+            link.download = suggestedName;
             document.body.append(link);
-            link.href = href;
+            link.href = canvas.toDataURL();
             link.click();
             link.remove();
-        }
-        download(png, "image.png");
+        };
+        saveCanvas(tmpCanvas, "image.png")
     }
     copyImg.src = blobURL;
 }
@@ -383,9 +360,9 @@ onMounted(() => {
     img.src = props.imgSrc;
 });
 
-watch(() => props.imgSrc, (newImgSrc) => img.src = props.imgSrc);
+watch(() => props.imgSrc, (newImgSrc) => img.src = newImgSrc);
 
-watch(() => props.param.backgroundColor, (newColor) => svg.style('background-color', newColor));
+watch(() => props.param.backgroundColor, (newColor) => svg?.style('background-color', newColor));
 
 // interestingly on HMR, this does not trigger because the running state is defined on parent
 watch(() => props.running, (newValue) => {
